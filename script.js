6b86aa2fbacba3498baa8c103c401df8cb5bc969
cerronfernando35-dev <cerronfernando35@gmail.com>
2026-03-30 19:09:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const text = "Edu kbrazo y agarra mi webo "; 
+const text = "edu gay "; 
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
