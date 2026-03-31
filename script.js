c3d5bd97d5e1dc710411235308f0614a994fbf36
cerronfernando35-dev <cerronfernando35@gmail.com>
2026-03-30 19:01:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const text = "I'll fuck peko "; 
+const text = "Edu kbrazo y agarra mi webo "; 
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
